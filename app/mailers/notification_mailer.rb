b872768fbc831775a77f3a9f547162501e2dbc8b
nerdycat99 <info@drysky.co.uk>
2017-11-01 11:31:25 +0000

    with email sending and template emailer view using variables to personalise emails

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,9 +2,11 @@ class NotificationMailer < ApplicationMailer
   default from:"no-reply@nomsterapp.com"
 
 
-  def comment_added
-    mail(to: "aaa@bbb.com",
-      subject: "Comment added to your place")
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: @place_owner.email,
+      subject: "Comment added to #{@place.name}")
   end
 
 end
